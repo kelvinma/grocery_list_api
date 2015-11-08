b163d774d4ae929e539eae962ed8ecd8783a001e
kelvinma <me@kelvinma.com>
2015-11-08 10:59:27 -0500

    Renamed type to typeof in Ingridents table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106210859) do
+ActiveRecord::Schema.define(version: 20151108155656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20151106210859) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string  "name"
-    t.string  "type"
+    t.string  "type_of"
     t.boolean "is_vegetarian"
     t.boolean "is_vegan"
   end
