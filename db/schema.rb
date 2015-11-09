7d4bb670fba9ac4b4c436c0320dcf19517b5e643
kelvinma <me@kelvinma.com>
2015-11-09 11:06:23 -0500

    Added Description Table to Recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108160251) do
+ActiveRecord::Schema.define(version: 20151109160420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,9 +54,9 @@ ActiveRecord::Schema.define(version: 20151108160251) do
     t.string "which_meal"
     t.string "style"
     t.text   "instructions"
+    t.text   "description"
   end
 
->>>>>>> migrations
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
     t.string   "token",           null: false
